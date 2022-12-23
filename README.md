9629cc69b18001e9ee3fa787bf5a69a1877b5dcf
skymim <ppt00003333@gmail.com>
2022-12-23 21:39:13 +0900

    upload files

---
+++ README.md
@@ -0,0 +1,5 @@
+# ParticleShape
+
+Express shapes easily with particles
+
+This plugin is required [ImageToParticle](https://github.com/sky-min/ImageToParticle).
--- src/Loader.php
+++ src/Loader.php
@@ -4,26 +4,17 @@ declare(strict_types=1);
 
 namespace skymin\ParticleShape;
 
-use pocketmine\event\EventPriority;
-use pocketmine\event\player\PlayerItemUseEvent;
 use pocketmine\plugin\PluginBase;
 use skymin\ImageParticle\particle\CustomParticle;
 use skymin\ImageParticle\particle\EulerAngle;
-use skymin\ParticleShape\shapes\Ellipse;
-use skymin\ParticleShape\shapes\Regular;
-use skymin\ParticleShape\shapes\Spiral;
-use skymin\ParticleShape\shapes\Star;
+use function class_exists;
 
 final class Loader extends PluginBase{
 
 	protected function onEnable() : void{
-		$this->getServer()->getPluginManager()->registerEvent(PlayerItemUseEvent::class, function(PlayerItemUseEvent $ev) : void{
-			$pos = $ev->getPlayer()->getLocation();
-			DrawParticle::draw(
-				EulerAngle::fromObject($pos, $pos->getWorld()),
-				(new CustomParticle())->setColor(mt_rand(0, 255), mt_rand(0, 255), mt_rand(0, 255)),
-				new Ellipse(6, 3)
-			);
-		}, EventPriority::MONITOR, $this);
+		if(!class_exists(EulerAngle::class) || !class_exists(CustomParticle::class)){
+			$this->getLogger()->critical('This plugin is required ImageToParticle.');
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+		}
 	}
 }
